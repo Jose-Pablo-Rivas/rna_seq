de552f5bedf145dc4fd2c9a5a43caa02614295b4
Jose-Pablo-Rivas <josemartin@molsysbio.us.es>
2019-11-13 15:41:59 +0100

    He cambiado el transcriptome_merging.sh para que meta los archivos en la carpeta results

--- transcriptome_merging.sh
+++ transcriptome_merging.sh
@@ -16,3 +16,6 @@ stringtie --merge -G $WD/annotation/annotation.gtf -o stringtie_merged.gtf $WD/l
 ## Comparing our assembly with the reference
 gffcompare -r $WD/annotation/annotation.gtf -G -o comparison stringtie_merged.gtf
 
+mv ../../sample_* $WD/results
+
+mv ../../transcriptome_merging.* $WD/results
